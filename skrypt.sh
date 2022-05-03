9bcb65ec143be3dc9f41bb5f63db1286f90dc388
Miłosz <milosz2781@gmail.com>
2022-05-03 21:08:44 +0200

    Aktualizacja skrypt.sh o dodawanie logów

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,14 @@ if [ "$1" == '--date' ]
 then
 	date
 fi
+
+if [ "$1" == '--logs' ]
+then
+       for i in $(seq 1 100);
+		do
+			touch log$i.txt;
+			echo log$i >> log$i.txt
+			echo $0 >> log$i.txt
+			date >> log$i.txt
+		done
+fi
