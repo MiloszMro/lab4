a748cd45497de5df2a5365f292cd27d2beee3949
Miłosz <milosz2781@gmail.com>
2022-05-03 21:27:41 +0200

    Aktualizacja skryptu o dodatkowe funkcjie przy log

--- skrypt.sh
+++ skrypt.sh
@@ -7,11 +7,24 @@ fi
 
 if [ "$1" == '--logs' ]
 then
-       for i in $(seq 1 100);
-		do
-			touch log$i.txt;
-			echo log$i >> log$i.txt
-			echo $0 >> log$i.txt
-			date >> log$i.txt
-		done
+	if [ -n "$2" ]
+	then
+       		for i in $(seq 1 $2);
+			do
+				touch log$i.txt;
+				echo log$i >> log$i.txt
+				echo $0 >> log$i.txt
+				date >> log$i.txt
+			done
+	
+
+	else
+       		for i in $(seq 1 100);
+			do
+				touch log$i.txt;
+				echo log$i >> log$i.txt
+				echo $0 >> log$i.txt
+				date >> log$i.txt
+			done
+	fi
 fi
